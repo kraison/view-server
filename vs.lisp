df4a486040998e9b9a93d46f96d32550e4a47ebc
Kevin Raison <raison@chatsubo.net>
2013-01-25 13:02:28 -0800

    delete unnecessary test file

--- vs.lisp
+++
@@ -1,8 +0,0 @@
-(ql:quickload :srn)
-(srn::start-server)
-(srn::delete-db)
-(srn::create-db)
-(srn::reload-views)
-(srn::populate-currencies "/home/raison/work/srn/src/oms/currency.txt")
-(srn::valid-currency? "USD")
-
